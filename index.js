44908c4923bef069fc7d2ff6f1b409b61411ee75
yobipratama <yobir231@gmail.com>
2023-02-19 20:57:33 +0700

    Full client Side Rendering

--- index.js
+++ index.js
@@ -1,37 +1,84 @@
-const linkHome = document.getElementById("Link-home");
-const linkAbout = document.getElementById("Link-about");
 const root = document.getElementById("root");
 
-linkHome.onclick = function (event){
-    event.preventDefault();
-    const homeScreen = HomeScreen();
-    root.innerHTML = "";
-    root.append(homeScreen); 
-    history.pushState(null, "", event.target.href);
-};
 
-linkAbout.onclick = function (event){
-    event.preventDefault();
-    root.textContent = "Welcome to About";
-    history.pushState(null, "", event.target.href);
-};
+function Navbar(){
+    const linkHome = document.createElement("a");
+    linkHome.href = "#home";
+    linkHome.textContent = "Home";
+    linkHome.onclick= function (event){
+        event.preventDefault();
+        const homeScreen = HomeScreen();
+        root.innerHTML = "";
+        root.append(homeScreen); 
+        history.pushState(null, "", event.target.href);
+    };
 
-function HomeScreen() { 
-    const input = document.createElement("input");
-    input.placeholder = "Enter your name"; 
+    const linkAbout = document.createElement("a");
+    linkAbout.href = "#about";
+    linkAbout.textContent = "About";
+    linkAbout.onclick= function (event){
+        event.preventDefault();
+        const aboutScreen = AboutScreen();
+        root.innerHTML = "";
+        root.append(aboutScreen); 
+        history.pushState(null, "", event.target.href);
+    };
 
-    const textPreview = document.createElement("p");
-    textPreview.textContent = "Hello";
-    
+    const div = document.createElement("div")
+    div.append(linkHome);
+    div.append(linkAbout);
+
+    return div;
+
+}
+
+function AboutScreen(){
+    const linkHome = document.createElement("a");
+    linkHome.href = "#home";
+    linkHome.textContent = "Kembali ke page Home";
+    linkHome.onclick= function(event){
+        event.preventDefault();
+        const homeScreen = HomeScreen();
+        root.innerHTML ="";
+        root.append(homeScreen); 
+        history.pushState(null, "", event.target.href);
+    };
+
+    const text = document.createElement("p");
+    text.textContent = "Welcome to About";
+
     const div = document.createElement("div");
-    div.append(input); 
-    div.append(textPreview); 
+    div.append(linkHome);
+    div.append(text);
 
     return div;
 }
 
+function HomeScreen(){
+    const navbar = Navbar();
+
+    const textPreview = document.createElement("p");
+
+    const input = document.createElement("input");
+    input.oninput = function (event) {
+        textPreview.textContent = event.target.value;
+    };
+    input.placeholder = "Enter your name";
+    
+    const div = document.createElement("div");
+    div.append(navbar);
+    div.append(input);
+    div.append(textPreview);
+
+    return div;
+}
+ 
 if (location.hash == "#about") {
-    root.textContent = "Welcome to About";
-} else if (location.hash == "#home") {
-    root.textContent = "Welcome to  Home";
+    const aboutScreen = AboutScreen();
+    root.innerHTML ="";
+    root.append(aboutScreen); 
+} else if (location.hash =="#home") {
+    const homeScreen = HomeScreen();
+    root.innerHTML = "";
+    root.append(homeScreen);
 }
