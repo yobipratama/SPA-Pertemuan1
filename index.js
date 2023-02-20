3fc80a26ab99c775c31da9e9ab4e98482f923c5f
yobipratama <yobir231@gmail.com>
2023-02-20 08:26:15 +0700

    commit

--- index.js
+++ index.js
@@ -1,48 +1,48 @@
-const root = document.getElementById("root");
+const root= document.getElementById("root");
 
+// jadi ketika ingin membuat link baru tinggal memanggil link berikut saja, supaya tidak mengulangi source code yg sama 
+function Link(props){
+    // parameter berupa ohject yaitu props, karena parameter nanti tidak selalu #home
+    const link = document.createElement("a");
+    link.href = props.href;
+    link.textContent = props.label;
+    link.onclick = function(event){
+        event.preventDefault();
+        const component = props.Component();
+        root.innerHTML = "";
+        root.append(component);
+        history.pushState(null, "", event.target.href);
+    };
+
+    return link
+}
 
 function Navbar(){
-    const linkHome = document.createElement("a");
-    linkHome.href = "#home";
-    linkHome.textContent = "Home";
-    linkHome.onclick= function (event){
-        event.preventDefault();
-        const homeScreen = HomeScreen();
-        root.innerHTML = "";
-        root.append(homeScreen); 
-        history.pushState(null, "", event.target.href);
-    };
+    const linkHome = Link({ 
+        href: "#home", 
+        label: "Home", 
+        Component: HomeScreen,
+    });
 
-    const linkAbout = document.createElement("a");
-    linkAbout.href = "#about";
-    linkAbout.textContent = "About";
-    linkAbout.onclick= function (event){
-        event.preventDefault();
-        const aboutScreen = AboutScreen();
-        root.innerHTML = "";
-        root.append(aboutScreen); 
-        history.pushState(null, "", event.target.href);
-    };
+    const linkAbout = Link({ 
+        href: "#about", 
+        label: "About", 
+        Component: AboutScreen,
+    });
 
     const div = document.createElement("div")
     div.append(linkHome);
     div.append(linkAbout);
 
     return div;
-
 }
 
 function AboutScreen(){
-    const linkHome = document.createElement("a");
-    linkHome.href = "#home";
-    linkHome.textContent = "Kembali ke page Home";
-    linkHome.onclick= function(event){
-        event.preventDefault();
-        const homeScreen = HomeScreen();
-        root.innerHTML ="";
-        root.append(homeScreen); 
-        history.pushState(null, "", event.target.href);
-    };
+    const linkHome = Link({ 
+        href: "#home", 
+        label: "Kembali ke home page", 
+        Component: HomeScreen,
+    });
 
     const text = document.createElement("p");
     text.textContent = "Welcome to About";
@@ -54,17 +54,17 @@ function AboutScreen(){
     return div;
 }
 
-function HomeScreen(){
+function HomeScreen() {
     const navbar = Navbar();
 
     const textPreview = document.createElement("p");
 
     const input = document.createElement("input");
-    input.oninput = function (event) {
+    input.oninput = function (event){
         textPreview.textContent = event.target.value;
-    };
+    }
     input.placeholder = "Enter your name";
-    
+
     const div = document.createElement("div");
     div.append(navbar);
     div.append(input);
@@ -72,12 +72,12 @@ function HomeScreen(){
 
     return div;
 }
- 
-if (location.hash == "#about") {
+
+if (location.hash == "#about"){
     const aboutScreen = AboutScreen();
-    root.innerHTML ="";
-    root.append(aboutScreen); 
-} else if (location.hash =="#home") {
+    root.innerHTML = "";
+    root.append(aboutScreen);
+} else if (location.hash == "#home"){
     const homeScreen = HomeScreen();
     root.innerHTML = "";
     root.append(homeScreen);
