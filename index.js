427ff84ad63adfbc686c9273b98e3464159cc61b
yobipratama <yobir231@gmail.com>
2023-02-20 09:09:32 +0700

    app component

--- index.js
+++ index.js
@@ -1,84 +1,79 @@
-const root= document.getElementById("root");
-
-// jadi ketika ingin membuat link baru tinggal memanggil link berikut saja, supaya tidak mengulangi source code yg sama 
-function Link(props){
-    // parameter berupa ohject yaitu props, karena parameter nanti tidak selalu #home
-    const link = document.createElement("a");
-    link.href = props.href;
-    link.textContent = props.label;
-    link.onclick = function(event){
-        event.preventDefault();
-        const component = props.Component();
-        root.innerHTML = "";
-        root.append(component);
-        history.pushState(null, "", event.target.href);
+function Link(props) {
+    const a = document.createElement("a");
+    a.href = props.href;
+    a.textContent = props.label;
+    a.onclick = function (event) {
+      event.preventDefault();
+      history.pushState(null, "", event.target.href);
+      render();
     };
-
-    return link
-}
-
-function Navbar(){
-    const linkHome = Link({ 
-        href: "#home", 
-        label: "Home", 
-        Component: HomeScreen,
-    });
-
-    const linkAbout = Link({ 
-        href: "#about", 
-        label: "About", 
-        Component: AboutScreen,
-    });
-
-    const div = document.createElement("div")
+    return a;
+  }
+  
+  function Navbar() {
+    const linkHome = Link({ href: "#home", label: "Home" });
+    const linkAbout = Link({ href: "#about", label: "About" });
+  
+    const div = document.createElement("div");
     div.append(linkHome);
     div.append(linkAbout);
-
+  
     return div;
-}
-
-function AboutScreen(){
-    const linkHome = Link({ 
-        href: "#home", 
-        label: "Kembali ke home page", 
-        Component: HomeScreen,
-    });
-
-    const text = document.createElement("p");
-    text.textContent = "Welcome to About";
-
-    const div = document.createElement("div");
-    div.append(linkHome);
-    div.append(text);
-
-    return div;
-}
-
-function HomeScreen() {
+  }
+  
+  function HomePage() {
     const navbar = Navbar();
-
+  
+    const p = document.createElement("p");
+    p.textContent = "Welcome to Home Page";
+  
     const textPreview = document.createElement("p");
-
+  
     const input = document.createElement("input");
-    input.oninput = function (event){
-        textPreview.textContent = event.target.value;
-    }
-    input.placeholder = "Enter your name";
-
+    input.placeholder = "enter your name";
+    input.oninput = function (event) {
+      textPreview.textContent = event.target.value;
+    };
+  
     const div = document.createElement("div");
     div.append(navbar);
+    div.append(p);
     div.append(input);
     div.append(textPreview);
-
+  
     return div;
-}
-
-if (location.hash == "#about"){
-    const aboutScreen = AboutScreen();
+  }
+  
+  function AboutPage() {
+    const linkHome = Link({ href: "#home", label: "Back to Home" });
+  
+    const p = document.createElement("p");
+    p.textContent = "Welcome to About Page";
+  
+    const div = document.createElement("div");
+    div.appendChild(linkHome);
+    div.appendChild(p);
+    return div;
+  }
+  
+  function App() {
+    const homePage = HomePage();
+    const aboutPage = AboutPage();
+  
+    if (window.location.hash == "#home") {
+      return homePage;
+    } else if (window.location.hash == "#about") {
+      return aboutPage;
+    } else {
+      return homePage;
+    }
+  }
+  
+  function render() {
+    const root = document.getElementById("root");
+    const app = App();
     root.innerHTML = "";
-    root.append(aboutScreen);
-} else if (location.hash == "#home"){
-    const homeScreen = HomeScreen();
-    root.innerHTML = "";
-    root.append(homeScreen);
-}
+    root.appendChild(app);
+  }
+  
+  render();
